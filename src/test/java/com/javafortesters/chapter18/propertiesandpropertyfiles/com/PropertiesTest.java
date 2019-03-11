ea77e166a0e6e0fe6752d9d45c1ef48a3f6e65fc
Elena <laskoskaelena@gmail.com>
2019-03-11 22:52:49 +0100

    Add Properties and Property Files Examples

---
+++ src/test/java/com/javafortesters/chapter18/propertiesandpropertyfiles/com/PropertiesTest.java
@@ -0,0 +1,48 @@
+package com.javafortesters.chapter18.propertiesandpropertyfiles.com;
+
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class PropertiesTest {
+    @Test
+    public void setProperties() {
+        Properties properties = new Properties();
+        assertThat(properties.size(), is(0));
+        properties.setProperty("browser", "chrome");
+        properties.setProperty("port", "80");
+        assertThat(properties.getProperty("browser"), is("chrome"));
+        assertThat(properties.getProperty("port"), is("80"));
+        assertThat(properties.getProperty("missing"), is(nullValue()));
+        assertThat(properties.getProperty("missing", "default"), is("default"));
+        for (String key : properties.stringPropertyNames()) {
+            System.out.println("Key:" + key + "" + "Value:" + properties.getProperty(key));
+        }
+        properties.list(System.out);
+        assertThat(properties.containsKey("browser"), is(true));
+    }
+
+    @Test
+    public void exerciseProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("name", "bob");
+        properties.setProperty("gender", "male");
+        properties.setProperty("password", "paSSword");
+        assertThat(properties.size(), is(3));
+        for (String key : properties.stringPropertyNames()) {
+            System.out.println("key:" + key + "" + "value:" + properties.getProperty(key));
+        }
+        properties.list(System.out);
+        assertThat(properties.containsKey("gender"), is(true));
+        assertEquals("bob", properties.get("name"));
+        assertThat(properties.getProperty("permission", "Admin"), is("Admin"));
+    }
+
+
+}
+
